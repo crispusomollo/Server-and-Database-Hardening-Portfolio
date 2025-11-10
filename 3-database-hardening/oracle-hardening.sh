a17621e5d87d6b20b365dd93e5693edd9832c3bc
crispusomollo <crispusomollo@gmail.com>
2025-11-10 10:03:27 +0300

    database hardening techniques

--- 3-database-hardening/oracle-hardening.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Oracle Hardening Script Placeholder
-
-echo "Please apply Oracle DB hardening steps manually."
-echo "1. Enforce strong passwords"
-echo "2. Remove unused accounts"
-echo "3. Enable auditing"
-
